1ef82ebf8ebc2e78070f0e929952328774e54ec3
Benjy Malca <bnmalcabis@gmail.com>
2020-03-23 20:56:15 -0500

    Se aniadieron tips para mejorar el proyecto.

---
+++ src/settings.py
@@ -0,0 +1,11 @@
+from os import getenv
+
+SETTINGS = {
+    "MYSQL_HOST": getenv('MYSQL_HOST', 'localhost'),
+    "MYSQL_USER": getenv('MYSQL_USER', 'root'),
+    "MYSQL_PASSWORD": getenv('MYSQL_PASSWORD', '1234'),
+    "MYSQL_DB": getenv('MYSQL_DB', 'flaskcontacs'),
+    "SECRET_KEY": getenv('SECRET_KEY', 'mysecretkey'),
+    "PORT": getenv('PORT', 8000),
+    "DEBUG": getenv('DEBUG', True)
+}
